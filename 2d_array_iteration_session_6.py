486e34e52b3d9f79496ba198ccc8b9bacc1cffbf
mattioiscattio <mattioiscattio@gmail.com>
2021-03-11 07:42:19 +0000

    removed extra

--- 2d_array_iteration_session_6.py
+++
@@ -1,8 +0,0 @@
-list = [[1,2,3,4,5,6],[11,12,13,14,15,16,],[21,22,23,24,25, 26],[31,32,33,34,35, 36]]
-totalValue = 0
-for arrayNum in range(len(list)):
-    for value in list[arrayNum]:
-        totalValue = totalValue + value
-    print(totalValue/len(list[arrayNum]), "this is the mean of row", arrayNum)
-    totalValue = 0
-    arrayNum += 1
--- Hours to seconds.py
+++
@@ -1,7 +0,0 @@
-h = int(input("enter the number of hours you want converted to seconds"))
-
-def convert(h):
-    s = h*3600
-    return s
-
-print(convert(h))
